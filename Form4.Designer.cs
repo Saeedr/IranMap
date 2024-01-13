ae206c98d33006ba7200c23e56778a15abc54db5
saeed rajayi <Saeedrajayi@gmail.com>
2024-01-13 15:54:13 +0330

    Add project files.

---
+++ Form4.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace IranMap
+{
+    partial class Form4
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pibIran = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblInfo = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pibIran)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pibIran
+            // 
+            this.pibIran.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.pibIran.Location = new System.Drawing.Point(12, 12);
+            this.pibIran.Name = "pibIran";
+            this.pibIran.Size = new System.Drawing.Size(132, 80);
+            this.pibIran.TabIndex = 1;
+            this.pibIran.TabStop = false;
+            this.pibIran.MouseLeave += new System.EventHandler(this.pibIran_MouseLeave);
+            this.pibIran.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pibIran_MouseMove);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.lblInfo);
+            this.panel1.Controls.Add(this.pibIran);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(457, 285);
+            this.panel1.TabIndex = 2;
+            // 
+            // lblInfo
+            // 
+            this.lblInfo.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblInfo.BackColor = System.Drawing.SystemColors.Control;
+            this.lblInfo.Location = new System.Drawing.Point(12, 209);
+            this.lblInfo.Name = "lblInfo";
+            this.lblInfo.Size = new System.Drawing.Size(433, 67);
+            this.lblInfo.TabIndex = 2;
+            this.lblInfo.Text = "label1";
+            // 
+            // Form4
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(457, 285);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form4";
+            this.Text = "Form4";
+            this.Load += new System.EventHandler(this.Form4_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pibIran)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pibIran;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lblInfo;
+    }
+}
---
+++ Form4.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace IranMap
+{
+    public partial class Form4 : Form
+    {
+        private MapInfo mapInfo;
+        private int CurrentIndex = -1;
+
+        public Form4()
+        {
+            InitializeComponent();
+        }
+
+        private void Form4_Load(object sender, EventArgs e)
+        {
+            mapInfo = new MapInfo(Application.StartupPath + "\\IranMap.set");
+
+            pibIran.Image = mapInfo.Main;
+            pibIran.Size = mapInfo.Main.Size;
+            this.Size = new Size(pibIran.Size.Width + 40, pibIran.Size.Height + 70 + lblInfo.Size.Height);
+        }
+
+        private void pibIran_MouseMove(object sender, MouseEventArgs e)
+        {
+            int Cur = mapInfo.InArea(e.X,e.Y);
+            if (Cur != CurrentIndex)
+            {
+                CurrentIndex = Cur;
+                if (CurrentIndex != -1)
+                {
+                    pibIran.Image = mapInfo.Hit(CurrentIndex);
+                    lblInfo.Text = mapInfo.GetDescription(CurrentIndex);
+                }
+                else
+                {
+                    pibIran.Image = mapInfo.Main;
+                    lblInfo.Text = "Iran Map";
+                }
+            }
+        }
+
+        private void pibIran_MouseLeave(object sender, EventArgs e)
+        {
+            pibIran.Image = mapInfo.Main;
+            CurrentIndex = -1;
+            lblInfo.Text = "";
+        }
+    }
+
+    internal class MapInfo
+    {
+        private Bitmap bmpMain = null;
+        private Bitmap bmpSecond = null;
+        private ItemMapInfo[] item;
+        private Color ColorMain;
+
+        public Bitmap Main { get{return bmpMain;}}
+        public Color Color { get { return ColorMain; } }
+
+        public MapInfo(string fileName)
+        {
+            string s;
+            int count,i;
+
+            using (TextReader file = new StreamReader(fileName))
+            {
+                s = file.ReadLine();
+                string[] ss = s.Split(new char[] { '\"' });
+                if (ss.Length < 2 && ss[0] != "MainPicture=")
+                    throw new Exception(string.Format("Format of file \'{0}\' is error!", fileName));
+                else
+                {
+                    bmpMain = new Bitmap(Image.FromFile(Path.GetDirectoryName(fileName) + "\\" + ss[1]));
+                    s = file.ReadLine();
+                    ss = s.Split(new char[] { '\"' });
+                    if (ss.Length < 2 && ss[0] != "SecondPicture=")
+                        throw new Exception(string.Format("Format of file \'{0}\' is error!", fileName));
+                    else
+                    {
+                        bmpSecond = new Bitmap(Image.FromFile(Path.GetDirectoryName(fileName) + "\\" + ss[1]));
+
+                        s = file.ReadLine();
+                        ss = s.Split(new char[] { ' ' });
+                        if (ss.Length < 4 && ss[0] != "Color=")
+                            throw new Exception(string.Format("Format of file \'{0}\' is error!", fileName));
+                        else
+                        {
+                            ColorMain = Color.FromArgb(Byte.Parse(ss[1]),Byte.Parse(ss[2]),Byte.Parse(ss[3]));
+                            s = file.ReadLine();
+                            ss = s.Split(new char[] { ' ' });
+                            if (ss.Length < 2 && ss[0] != "Count=")
+                                throw new Exception(string.Format("Format of file \'{0}\' is error!", fileName));
+                            else
+                            {
+                                count = Int32.Parse(ss[1], System.Globalization.NumberStyles.Integer);
+
+                                item = new ItemMapInfo[count];
+
+                                for (i = 0; i < count; i++)
+                                {
+                                    s = file.ReadLine();
+                                    ss = s.Split(new char[] { ' ' });
+                                    if (ss.Length < 7)
+                                        throw new Exception(string.Format("Format of file \'{0}\' is error!", fileName));
+                                    else
+                                    {
+                                        item[i].DrawX = Int32.Parse(ss[0]);
+                                        item[i].DrawY = Int32.Parse(ss[1]);
+                                        item[i].BeginX = Int32.Parse(ss[2]);
+                                        item[i].BeginY = Int32.Parse(ss[3]);
+                                        item[i].Width = Int32.Parse(ss[4]);
+                                        item[i].Height = Int32.Parse(ss[5]);
+                                        item[i].Text = ss[6];
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        public int InArea(int x, int y)
+        {
+            int i,j,k;
+            Color c;
+            k = 0;
+            foreach (ItemMapInfo imi in item)
+            {
+                i = x - imi.DrawX;
+                j = y - imi.DrawY;
+                if (i >= 0 && i <= imi.Width && j >= 0 && j <= imi.Height)
+                {
+                    i += imi.BeginX;
+                    j += imi.BeginY;
+                    if (i < bmpSecond.Width && j < bmpSecond.Height)
+                    {
+                        c = bmpSecond.GetPixel(i, j);
+                        if (c.A != 0)
+                        {
+                            return k;
+                        }
+                    }
+                }
+                k++;
+            }
+            return -1;
+        }
+
+        public Image Hit(int Index)
+        {
+            Bitmap bmpHelper = new Bitmap(Main);
+            ItemMapInfo imi = item[Index];
+
+            using (Graphics g = Graphics.FromImage(bmpHelper))
+            {
+                g.DrawImage(bmpSecond, new Rectangle(imi.DrawX, imi.DrawY, imi.Width, imi.Height),
+                               imi.BeginX, imi.BeginY, imi.Width, imi.Height, GraphicsUnit.Pixel);
+            }
+            return bmpHelper;
+        }
+
+        internal string GetDescription(int Index)
+        {
+            return item[Index].Text;
+        }
+    }
+
+    internal struct ItemMapInfo
+    {
+        public int DrawX, DrawY;
+        public int Width, Height;
+        public int BeginX, BeginY;
+        public string Text;
+    }
+}
